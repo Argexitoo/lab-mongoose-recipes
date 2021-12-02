d2da98f8b18899e7db83ef6615f24bc96609820a
Argexitoo <argexitoo@gmail.com>
2021-12-02 22:05:37 +0100

    done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,6 +22,43 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Pizza",
+      level: "Easy Peasy",
+      ingredients: ["Cheese","Tomato","Pizza dough","Ham"],
+      cuisine: "Italian",
+      duration: 60,
+    })
+  })
+  .then((newRecipe) => {
+    console.log(newRecipe.title)
+  })
+  .then(() => Recipe.insertMany(data))
+  .then((newList) => {
+    newList.forEach((newList) => {
+      console.log(newList.title)
+      })
+    })
+  .then(() => {
+     return Recipe.findOneAndUpdate(
+      {title: "Rigatoni alla Genovese"},
+      {duration: 100},
+      {new: true},
+    )
+  })
+  .then((newDuration) => {
+    console.log("New Duration:", newDuration.duration);
+  })
+  .then(() => {
+    return Recipe.deleteOne(
+      {title: "Carrot Cake"}
+    )
+  })
+  .then((deleted) => {
+    console.log("Recipe Deleted:", deleted)
+  })
+  .then(() => {
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
